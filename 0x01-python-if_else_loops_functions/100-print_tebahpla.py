6b69c5dd9d5e7d4ce0284fbd5f64d46e2d2063b0
zakariasan <elitziko@gmail.com>
2023-10-04 02:06:32 +0100

    print tbeah

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
-for c in range(122, 98, -1):
-    print("{:s}".format(chr(c)), end='')
+for c in range(122, 96, -1):
+    if (c % 2 != 0):
+        print("{:s}".format(chr(c - 32)), end='')
+    else:
+        print("{:s}".format(chr(c)), end='')
