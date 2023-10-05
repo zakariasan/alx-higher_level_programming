f4c663d78114d46e11d2055835c139b790a108fc
zakariasan <elitziko@gmail.com>
2023-10-06 00:57:31 +0100

    calculate me

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from calculator_1.py import add, div, sub, mul
+    import sys
+
+if (len(sys.argv) - 1 != 3):
+    print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+    exit(1)
+
+fun_ops = {'+': add, '/': div, '*': mul, '-': sub}
+
+if (sys.argv[2] not in fun_ops):
+    print("Unknown operator. Available operators: +, -, * and /")
+    exit(1)
+
+a = int(sys.argv[1])
+b = int(sys.argv[3])
+print("{} {} {} = {}".format(a, sys.argv[2], b, fun_ops[sys.argv[2]]))
