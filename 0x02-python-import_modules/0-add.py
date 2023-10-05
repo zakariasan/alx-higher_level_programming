00770a60ee0c1f8b02cf72ac2776312dee00c5bb
zakariasan <elitziko@gmail.com>
2023-10-05 21:46:11 +0100

    add error a and b

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import add_0
-print("{:d} + {:d} = {:d}".format(1, 2, add_0.add(1, 2)))
+
+
+a = 1
+b = 2
+print("{:d} + {:d} = {:d}".format(a, b, add_0.add(1, 2)))
