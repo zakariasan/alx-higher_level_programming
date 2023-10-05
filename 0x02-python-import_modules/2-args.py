a081a6a9d901d0e9f8b06d0e4899d4803ed67f38
zakariasan <elitziko@gmail.com>
2023-10-05 23:29:45 +0100

    arfs

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,10 +2,10 @@
 if __name__ == "__main__":
     import sys
 
-if (len(sys.argv) - 1 != 0):
-    print("{} arguments:".format(len(sys.argv) - 1))
+if (len(sys.argv) - 1 == 0):
+    print("0 arguments.")
 elif (len(sys.argv) - 1 == 1):
-    print("1 arguments.")
+    print("1 arguments:")
 else:
     print("{} arguments.".format(len(sys.argv) - 1))
 
