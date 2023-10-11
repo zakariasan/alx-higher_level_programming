1ca6e71f3fcc0114e9f9ed983ab63671840e0d88
zakariasan <elitziko@gmail.com>
2023-10-12 00:28:39 +0100

    complexe type

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def complex_delete(a_dictionary, value):
-    a_dictionary = {key: val for key, val in a_dictionary.item() if val !=
+    a_dictionary = {key: val for key, val in a_dictionary.items() if val !=
                     value}
     return a_dictionary
