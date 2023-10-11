b615f97cc7457ad3f5027560b36a311671dea926
zakariasan <elitziko@gmail.com>
2023-10-12 00:06:03 +0100

    complexe time

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
 def complex_delete(a_dictionary, value):
-    return {key: val for key, val in a_dictionary.item() if val != value}
+    a_dictionary = {key: val for key, val in a_dictionary.item() if val !=
+                    value}
+    return a_dictionary
