ad704f71ca7bd502327477d58fdf3472d25239e9
zakariasan <elitziko@gmail.com>
2023-10-11 23:11:02 +0100

    weight average

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    if len(my_list) > 0:
+        up = 0
+        down = 0
+        for gr in my_list:
+            up += (gr[0] * gr[1])
+            down += (gr[1])
+            return (up/down)
+    return (0)
