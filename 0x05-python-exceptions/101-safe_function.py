b11b6651da8b7ea15d2c00273033c43491c72ec6
zakariasan <elitziko@gmail.com>
2023-10-24 00:21:12 +0100

    safe me

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import sys
+
+
+def safe_function(fct, *args):
+    try:
+        allin = fct(*args)
+    except Exception:
+        print("Exception: {}".format(Exception), file=sys.stderr)
+        return None
+    return allin
