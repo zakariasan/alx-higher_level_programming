96c4bf0277877ef9f92b159913b66acd6b32c524
zakariasan <elitziko@gmail.com>
2023-10-24 00:47:04 +0100

    magic happed

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -6,7 +6,7 @@ def safe_function(fct, *args):
     try:
         allin = fct(*args)
     except Exception:
-        print("Exception: {}".format(Exception), file=sys.stderr)
+        print("Exception: {}".format(Exception.args[0]), file=sys.stderr)
         return None
     else:
         return allin
--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
-import sys
-
-
-def safe_function(fct, *args):
-    try:
-        allin = fct(*args)
-    except Exception:
-        print("Exception: {}".format(Exception.args[0]), file=sys.stderr)
-        return None
-    return allin
+def magic_calculation(a, b):
+    result = 0
+    for i in range(1, 3):
+        try:
+            if (i > a):
+                raise Exception("Too far")
+            else:
+                result += (a ** b) / i
+        except Exception:
+            result = b + a
+            break
+        return result
