31764a91c1992446beb1fccd9e18b67d056609b2
zakariasan <elitziko@gmail.com>
2023-10-25 00:12:28 +0100

    square py and __setter

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""Class of a square with size"""
+
+
+class Square:
+    """class content private size"""
+
+    def __init__(self, size=0):
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0 ")
+        else:
+            self.__size = size
+
+    def area(self):
+        return self.__size * self.__size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0 ")
+        else:
+            self.__size = value
