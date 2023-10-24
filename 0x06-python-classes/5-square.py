f984bc6851ead209d1b576d2ceee685955148734
zakariasan <elitziko@gmail.com>
2023-10-25 00:17:26 +0100

    printing # square

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""Class of a square with size"""
+
+
+class Square:
+    """class content private size"""
+
+    def __init__(self, size=0):
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0 ")
+        else:
+            self.__size = size
+
+    def area(self):
+        return self.__size * self.__size
+
+    def my_print(self):
+        for x in range(0, self.__size):
+            for y in range(0, self.__size):
+                print("#", end='')
+            print('')
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0 ")
+        else:
+            self.__size = value
