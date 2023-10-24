e1e4f1f7bf3a493b4da35f3890a020386e8282e4
zakariasan <elitziko@gmail.com>
2023-10-25 00:03:16 +0100

    sie pof

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -6,7 +6,7 @@ class Square:
     """class content private size"""
 
     def __init__(self, size=0):
-        if type(size) != int:
+        if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0 ")
