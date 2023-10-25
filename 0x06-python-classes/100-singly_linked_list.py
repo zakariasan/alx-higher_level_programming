268f11bbbfa9847b537a6b2bba6127fef8a9df2b
zakariasan <elitziko@gmail.com>
2023-10-25 03:27:10 +0100

    square py

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -48,7 +48,7 @@ class SinglyLinkedList:
             self.__head = item
         else:
             tmp = self.__head
-            while tmp.next_node is not None and tmp.next_node.data < value:
+            while tmp.next_node is not None and tmp.next_node.data <= value:
                 tmp = tmp.next_node
             item.next_node = tmp.next_node
             tmp.next_node = item
---
+++ 0x06-python-classes/101-square.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python3
+"""Class of a square with size"""
+
+
+class Square:
+    """class content private size"""
+
+    def __init__(self, size=0, position=(0, 0)):
+        self.__size = size
+        self.position = position
+
+    def area(self):
+        return self.__size * self.__size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @property
+    def position(self):
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        if type(value) != tuple or len(value) != 2 or type(value[0]) != int or\
+                type(value[1]) != int or value[0] < 0 or value[1] < 0:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        else:
+            self.__position = value
+
+    @size.setter
+    def size(self, value):
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0 ")
+        else:
+            self.__size = value
+
+    def my_print(self):
+        if not self.__size:
+            print('')
+        else:
+            for x in range(0, self.__position[1]):
+                print("")
+            for x in range(0, self.__size):
+                print(" " * self.__position[0], end='')
+                print("#" * self.__size)
+
+    def __str__(self):
+        lst = []
+        if not self.__size:
+            return ''
+        else:
+            for x in range(0, self.__position[1]):
+                lst.append('\n')
+            for x in range(0, self.__size):
+                lst.append(" " * self.__position[0])
+                lst.append("#" * self.__size)
+                if (x < self.__size - 1):
+                    lst.append('\n')
+            return ''.join(lst)
