bd845d03923446b8476bc6d60891f66c4416ea61
zakariasan <elitziko@gmail.com>
2023-10-25 02:55:44 +0100

    singly linked list

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -41,13 +41,14 @@ class SinglyLinkedList:
     def sorted_insert(self, value):
         item = Node(value)
         if self.__head is None:
+            item.next_node = None
             self.__head = item
         elif self.__head.data > value:
             item.next_node = self.__head
             self.__head = item
         else:
             tmp = self.__head
-            while tmp.next_node is not None and tmp.next_node.data <= value:
+            while tmp.next_node is not None and tmp.next_node.data < value:
                 tmp = tmp.next_node
             item.next_node = tmp.next_node
             tmp.next_node = item
