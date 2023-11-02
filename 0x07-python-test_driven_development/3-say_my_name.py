0fe90f75451ffcb8d6b2dd94c553f349ea9bc4d8
zakariasan <elitziko@gmail.com>
2023-11-02 17:01:54 +0100

    my name func fix

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -3,8 +3,10 @@
 
 
 def say_my_name(first_name, last_name=""):
+    """You can say my Name please"""
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
     elif not isinstance(last_name, str):
         raise TypeError("first_name must be a string")
-    print("My name is {} {}".format(first_name, last_name))
+    else:
+        print("My name is {} {}".format(first_name, last_name))
