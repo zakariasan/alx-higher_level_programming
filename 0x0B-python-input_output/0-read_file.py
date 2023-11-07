47efffc8d0bacd8e6e31437b8a60f2c75e4db48b
zakariasan <elitziko@gmail.com>
2023-11-07 14:38:11 +0100

    read file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,6 +5,6 @@
 
 
 def read_file(filename=""):
+    """ read file """
     with open(filename, encoding="utf-8") as f:
-        for line in f:
-            print(line, end='')
+        print(f.read(), end='')
