b43e79fb0ba95c2821d8d26d403fb8121ddfee30
zakariasan <elitziko@gmail.com>
2023-11-07 20:31:12 +0100

    100 append after

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -6,8 +6,9 @@
 
 def append_after(filename="", search_string="", new_string=""):
     """ insert txt to file """
-    with open(filename, 'wr') as target:
+    with open(filename, 'r') as target:
         lines = target.readlines()
+    with open(filename, 'w') as target:
         for line in lines:
             target.write(line)
             if search_string in line:
