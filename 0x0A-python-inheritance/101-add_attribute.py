b0c226ae2ca3bda32d8f962afa4d44c2af1be109
zakariasan <elitziko@gmail.com>
2023-11-07 03:13:25 +0100

    empty attr func

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -5,6 +5,6 @@
 def add_attribute(obj, attribute, res):
     """add new att if not exit"""
 
-    if (hasattr(obj, "__dict__")):
+    if (not hasattr(obj, "__dict__")):
         raise TypeError("can't add new attribute")
     setattr(obj, attribute, res)
