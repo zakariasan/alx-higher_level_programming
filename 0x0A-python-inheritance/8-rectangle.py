9f48473b05130d0fbbfb842e3afa791e3a416c4e
zakariasan <elitziko@gmail.com>
2023-11-07 01:33:27 +0100

    recta

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""BaseGeometry"""
+
+
+class BaseGeometry:
+    """BaseGeometry"""
+
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+        elif value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+
+
+class Rectangle(BaseGeometry):
+    """Rectangle base"""
+
+    def __init__(self, width, height):
+        if self.integer_validator("width", width)\
+                and self.integer_validator("height", height):
+            self.__width__ = width
+            self.__height__ = height
