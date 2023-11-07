c486865072b345a939406e97701c3cba3566d9f9
zakariasan <elitziko@gmail.com>
2023-11-07 02:17:26 +0100

    try catch try

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -20,7 +20,7 @@ class Rectangle(BaseGeometry):
 
     def __init__(self, width, height):
         super().__init__()
-        self.integer_validator("width", width)
-        self.integer_validator("height", height)
         self.__width = width
         self.__height = height
+        self.integer_validator("width", self.__width)
+        self.integer_validator("height", self.__height)
