cbe8ee315c30023e58524cceb3e2fba06cf18659
zakariasan <elitziko@gmail.com>
2023-11-07 01:48:43 +0100

    sqaure

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -19,7 +19,14 @@ class Rectangle(BaseGeometry):
     """Rectangle base"""
 
     def __init__(self, width, height):
+        super().__init__()
         if self.integer_validator("width", width)\
                 and self.integer_validator("height", height):
             self.__width = width
             self.__height = height
+
+    def area(self):
+        return self.__width * self.__height
+
+    def __str__(self):
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
