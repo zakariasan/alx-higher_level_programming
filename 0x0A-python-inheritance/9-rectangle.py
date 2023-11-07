171c40e267718e9871939e9d19bef1298833e92a
zakariasan <elitziko@gmail.com>
2023-11-07 02:35:51 +0100

    reactange 9

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -1,32 +1,20 @@
 #!/usr/bin/python3
 """BaseGeometry"""
-
-
-class BaseGeometry:
-    """BaseGeometry"""
-
-    def area(self):
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        if not isinstance(value, int):
-            raise TypeError("{} must be an integer".format(name))
-        elif value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
     """Rectangle base"""
 
     def __init__(self, width, height):
-        super().__init__()
-        if self.integer_validator("width", width)\
-                and self.integer_validator("height", height):
-            self.__width = width
-            self.__height = height
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+
+        self.__width = width
+        self.__height = height
 
     def area(self):
         return self.__width * self.__height
 
     def __str__(self):
-        return "[Rectangle] {}/{}".format(self.__width, self.__height)
+        return "[Rectangle] {:d}/{:d}".format(self.__width, self.__height)
