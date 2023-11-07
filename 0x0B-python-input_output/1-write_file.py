b5e9695918900709e6cef9b4abe65fa61c1d3f76
zakariasan <elitziko@gmail.com>
2023-11-07 14:43:39 +0100

    write file

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+    function that writes a str to file (UTF8) and prints it to stdout:
+"""
+
+
+def write_file(filename="", text=""):
+    """ writ file """
+    with open(filename, 'w', encoding="utf-8") as f:
+        f.write(text)
