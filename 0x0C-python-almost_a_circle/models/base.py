b3b3210700a01b575b1a435516360a679b5acff3
zakariasan <elitziko@gmail.com>
2023-11-14 00:19:43 +0100

    base json

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -21,7 +21,7 @@ class Base:
     def to_json_string(list_dictionaries):
         """desc of json to string """
         if (list_dictionaries is None):
-            return []
+            return "[]"
         return json.dumps(list_dictionaries)
 
     @classmethod
