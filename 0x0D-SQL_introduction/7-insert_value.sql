af8c89ad69836741039cb69c0fd094253bccef05
zakariasan <elitziko@gmail.com>
2023-12-12 21:12:06 +0100

    SQL - Introduction

---
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -0,0 +1,4 @@
+-- Insert values into first_table
+INSERT INTO first_table (id, name)
+VALUES (89, 'Holberton School');
+
---
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -0,0 +1,5 @@
+-- Count rows with id 89 in first_table
+SELECT COUNT(*) AS count_89
+FROM first_table
+WHERE id = 89;
+
