7f0b8d49aa557d07e5b0b1e4ef19831db009cb38
zakariasan <elitziko@gmail.com>
2023-12-12 22:16:01 +0100

    SQL - Introduction

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,6 +1,5 @@
 -- Convert the database to UTF8
--- Convert the table first_table to UTF8
--- Convert the field name in first_table to UTF8
-ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-ALTER TABLE first_table MODIFY COLUMN name VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE first_table
+MODIFY COLUMN name VARCHAR(255)
+CHARACTER SET utf8mb4
+COLLATE utf8mb4_unicode_ci;
--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -4,11 +4,9 @@ SELECT
     AVG(value) AS avg_temp
 FROM
     temperatures
-WHERE
-    MONTH(date) IN (7, 8)
+WHERE month = 7 OR month = 8
 GROUP BY
     city
 ORDER BY
     avg_temp DESC
 LIMIT 3;
-
