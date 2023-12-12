3c59e657437efde31b819abbc136a4965f7d62d6
zakariasan <elitziko@gmail.com>
2023-12-12 22:22:32 +0100

    SQL - Introduction2

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,10 +1,5 @@
 --Write a script that displays the average temperature
-SELECT
-    city,
-    AVG(value) AS avg_temp
-FROM
-    temperatures
-GROUP BY
-    city
-ORDER BY
-    avg_temp DESC;
+SELECT city, AVG(value) AS avg_temp
+FROM temperatures
+GROUP BY city
+ORDER BY avg_temp DESC;
