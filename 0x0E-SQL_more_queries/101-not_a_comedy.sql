650b62f4781a240ae91b2cef1aae69cd8fa4986c
zakariasan <elitziko@gmail.com>
2023-12-14 03:05:19 +0100

    0x0E. SQL - More queries3

--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -2,7 +2,7 @@
 
 SELECT tv_shows.title
 FROM tv_shows
-WHERE tv_shows.title = 'Dexter' AND tv_shows.title NOT IN (
+WHERE tv_shows.title NOT IN (
     SELECT tv_shows.title
     FROM tv_shows
     INNER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
