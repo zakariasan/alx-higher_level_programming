311805fd96424c20310c56bff1507f501a5cdb32
zakariasan <elitziko@gmail.com>
2024-01-25 23:55:08 +0100

    permessions

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #Write a Bash script that takes in a URL, sends a request to that URL
-curl -sI "$1" | grep -i "Content-Length" | awk '{print $2}' | tr -d '\r\n'
+curl -sI "$1" | grep -i "Content-Length" | cut -d " " -f 2 
