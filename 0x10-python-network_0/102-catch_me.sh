0f94332aae88ab45f38b38ddaa4e61621832a98b
zakariasan <elitziko@gmail.com>
2024-01-26 00:05:05 +0100

    catch me

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
 #Write a Bash script that takes in a URL, sends a request to that URL
-response=$(curl -s 0.0.0.0:5000/catch_me)
-
-if [[ $response == *"You got me!"* ]]; then
-    printf "Server response: You got me!\n"
-else
-    printf "Error: Server response does not contain the expected message.\n"
-fi
+curl -X PUT -d "You got me!" 0.0.0.0:5000/catch_me
