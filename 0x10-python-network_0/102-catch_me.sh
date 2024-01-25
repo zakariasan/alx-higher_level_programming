f4324e4966987589dfb44f762260bbb016ba235a
zakariasan <elitziko@gmail.com>
2024-01-26 00:09:41 +0100

    catch me

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #Write a Bash script that takes in a URL, sends a request to that URL
-curl -X PUT -d "You got me!" 0.0.0.0:5000/catch_me
+curl -s -X PUT --data "You got me!" 0.0.0.0:5000/catch_me > /dev/null
