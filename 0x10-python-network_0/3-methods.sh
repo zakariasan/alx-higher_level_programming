310afd6af1c399348556b1fa35cc018d884da9da
zakariasan <elitziko@gmail.com>
2024-01-26 00:07:00 +0100

    meth

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #Write a Bash script that takes in a URL and displays all HTTP
-curl -s -I -X OPTIONS "$1" | grep "Allow:" | cut -d " " -f 2
+curl -s -I -X OPTIONS "$1" | grep "Allow:" | cut -d " " -f2-
