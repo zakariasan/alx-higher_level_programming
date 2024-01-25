45360c384b8c4175858ac39cb56494095895e1eb
zakariasan <elitziko@gmail.com>
2024-01-25 23:26:05 +0100

    sneak pik

---
+++ 0x10-python-network_0/5-post_params.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+curl -s "$1" -X POST -d "email=test@gmail.com&subject=I will always be here for PLD"
---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Write a function that finds a peak in a list of unsorted integers."""
+
+
+def find_peak(list_of_integers):
+    """
+    Find a peak in a list of unsorted integers.
+        int or None: The peak value, or None if the list is empty.
+    """
+    if not list_of_integers:
+        return None
+
+    low, high = 0, len(list_of_integers) - 1
+
+    while low < high:
+        mid = (low + high) // 2
+
+        if list_of_integers[mid] > list_of_integers[mid + 1]:
+            high = mid
+        else:
+            low = mid + 1
+
+    return list_of_integers[low]
