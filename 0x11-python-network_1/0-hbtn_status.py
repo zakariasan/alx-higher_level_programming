b01d72b2d7b7eb7bc17794af0c04fcb94170c8c8
zakariasan <elitziko@gmail.com>
2024-01-26 23:03:02 +0100

    status by

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,11 +2,11 @@
 """Write a Python script that fetches"""
 import urllib.request
 
-url = urllib.request.Request('https://alx-intranet.hbtn.io/status')
-
-with urllib.request.urlopen(url) as response:
-    html_content = response.read()
-    print("Body response:")
-    print("\t- type: {}".format(type(html_content)))
-    print("\t- content: {}".format(html_content))
-    print("\t- utf8 content: {}".format(html_content.decode('utf-8')))
+if __name__ == "__main__":
+    url = urllib.request.Request('https://alx-intranet.hbtn.io/status')
+    with urllib.request.urlopen(url) as response:
+        html_content = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(html_content)))
+        print("\t- content: {}".format(html_content))
+        print("\t- utf8 content: {}".format(html_content.decode('utf-8')))
