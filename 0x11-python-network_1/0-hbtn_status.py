235ec67e38a6ea8d05b7a8c9eadbe7840834a1dd
zakariasan <elitziko@gmail.com>
2024-01-26 22:58:23 +0100

    hbtn stats

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -4,10 +4,9 @@ import urllib.request
 
 url = 'https://alx-intranet.hbtn.io/status'
 
-if __name__ == "__main__":
-    with urllib.request.urlopen(url) as response:
-        html_content = response.read()
-        print("Body response:")
-        print("\t- type: {}".format(type(html_content)))
-        print("\t- content: {}".format(html_content))
-        print("\t- utf8 content: {}".format(html_content.decode('utf-8')))
+with urllib.request.urlopen(url) as response:
+    html_content = response.read()
+    print("Body response:")
+    print("         - type: {}".format(type(html_content)))
+    print("         - content: {}".format(html_content))
+    print("         - utf8 content: {}".format(html_content.decode('utf-8')))
