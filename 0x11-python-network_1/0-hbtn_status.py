691f654e175d6dc6df41af220134b482cbbacfa5
zakariasan <elitziko@gmail.com>
2024-01-26 22:11:43 +0100

    hbtn status

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Write a Python script that fetches"""
+import urllib.request
+
+url = 'https://alx-intranet.hbtn.io/status'
+
+with urllib.request.urlopen(url) as response:
+    html_content = response.read()
+    print("Body response:")
+    print("\t- type:", type(html_content))
+    print("\t- content:", html_content.decode('utf-8'))
