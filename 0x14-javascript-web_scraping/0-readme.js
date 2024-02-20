0e85c267fa82dbf88dd6db8931d07b2b4f1161a7
zakariasan <elitziko@gmail.com>
2024-02-20 23:49:31 +0100

    fix: semistand

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,18 +1,18 @@
 #!/usr/bin/node
 const fs = require('fs');
 
-function readFile(f_path) {
-	fs.readFile(f_path, 'utf8', (err, data)=> {
-		if (err) {
-			console.log(err);
-			return;
-		}
-		console.log(data);
-	})
+function readFile (fPath) {
+  fs.readFile(fPath, 'utf8', (err, data) => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    console.log(data);
+  });
 }
 
-if (process.argv.length != 3) {
-	console.log("Usage: ./0-readme.js <file_path>");
+if (process.argv.length !== 3) {
+  console.log('Usage: ./0-readme.js <file_path>');
 } else {
-	readFile(process.argv[2]);
+  readFile(process.argv[2]);
 }
